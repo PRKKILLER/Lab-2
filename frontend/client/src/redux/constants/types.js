f1e43a77c5ab37f93b5a91ffb09d43ddb6f20b22
PRKKILLER <36620963+PRKKILLER@users.noreply.github.com>
2021-04-25 21:21:11 -0700

    redux setup

--- frontend/client/src/redux/constants/types.js
+++ frontend/client/src/redux/constants/types.js
@@ -5,3 +5,23 @@ export const signup = 'signup';
 export const logout = 'logout';
 export const unauthenticated = 'unauthenticated';
 export const authenticated = 'authenticated';
+export const GETGROUP = 'GETGROUP';
+export const ACCEPTINVITATION = 'ACCEPTINVITATION';
+export const LEAVEGROUP = 'LEAVEGROUP';
+//  activity PAGE ACTIONS
+export const GETACTIVITIES = 'GETACTIVITIES';
+// INDIVIDUAL GROUP PAGE ACTIONS
+export const CREATENOTE = 'CREATENOTE';
+export const DELETENOTE = 'DELETENOTE';
+export const GETNOTE = 'GETNOTE';
+export const SHOWEXPANSE = 'SHOWEXPANSE';
+export const ADDEXPENSE = 'ADDEXPENSE';
+export const GETGROUPSUMMARY = 'GETGROUPSUMMARY';
+// DASHBOARD PAGE ACTIONS
+export const SETTLEUP = 'SETTLEUP';
+export const GETUSEROWES = 'GETUSEROWES';
+export const GETUSEROWED = 'GETUSEROWED';
+// create group no use in redux
+export const GETUSERS = 'GETUSERS';
+export const CREATEGROUP = 'CREATEGROUP';
+export const GROUPEPICTURE = 'GROUPEPICTURE';
--- frontend/client/src/redux/reducers/rootReducer.js
+++ frontend/client/src/redux/reducers/rootReducer.js
@@ -1,10 +1,20 @@
 import { combineReducers } from 'redux';
 import authReducer from './authReducer';
 import profileReducer from './profileReducer';
+import myGroupReducer from './mygroupReducer';
+import recentActivitiesReducer from './recentActivityReducer';
+import individualGroupReducer from './individualGroupReducer';
+import dashboardReducer from './dashboardReducer';
+import createGroupReducer from './createGroupReducer';
 
 const rootReducer = combineReducers({
   auth: authReducer,
   profile: profileReducer,
+  group: myGroupReducer,
+  recentActivity: recentActivitiesReducer,
+  individualGroup: individualGroupReducer,
+  dashboard: dashboardReducer,
+  createGroup: createGroupReducer,
 });
 
 export default rootReducer;
